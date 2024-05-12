4ba87c07ad32d2ef9a9b3d2ec9865d074b295641
vishal <rdshrma61@mail.com>
2024-05-12 12:58:41 +0530

    Created API's like foodService in nodejs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { sample_foods, sample_tags } from "./data";
 
 const app = express();
 const port = 5000;
@@ -10,9 +11,36 @@ app.use(cors({
 
 }));
 
-app.get('/api/foods', (req, resp) => {
-  resp.send("Hi there")
+app.get('/api/foods', (req, resp) => { // replicate to getAll function()
+  resp.send(sample_foods)
 });
+
+app.get('/api/foods/search/:searchTerm', (req, resp) => { // searching method
+  const searchTerm = req.params.searchTerm;
+  const foods = sample_foods.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  resp.send(foods);
+});
+
+app.get('/api/foods/tags', (req, resp) => { // getting by tags
+  resp.send(sample_tags)
+});
+
+app.get('/api/foods/tags/:tagName', (req, resp) => {
+  const tagName = req.params.tagName;
+  const foods = sample_foods.filter(item =>
+    item.tags?.includes(tagName));
+  resp.send(foods);
+});
+
+app.get('/api/foods/:FoodId', (req, resp) => {
+  const foodId = req.params.FoodId;
+  const foods = sample_foods.find(item => item.id === foodId);
+
+  resp.send(foods);
+})
+
 
 
 app.listen(5000, () => {
