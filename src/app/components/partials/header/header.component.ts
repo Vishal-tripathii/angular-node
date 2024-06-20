2ede30e72af0f764723f67444fa21289e838ca66
Vishal Tripathi <rdshrma61@gmail.com>
2024-06-20 16:51:50 +0530

    added local storage

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { FavoriteService } from 'src/app/services/favorite.service';
 import { FoodService } from 'src/app/services/food.service';
+import { UserService } from 'src/app/services/user.service';
 import { Food } from 'src/app/shared/models/food';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +15,18 @@ export class HeaderComponent implements OnInit {
 
   cartQuantity: number = 0;
   favoriteQuantity: number = 0;
+  user!: User
 
   constructor(
     private _cartServices: CartService,
     private _favoriteService: FavoriteService,
-    private _foodService: FoodService
+    private _foodService: FoodService, 
+    private _userService: UserService
   ) {
+
+    this._userService.userObservable.subscribe(newUser => {
+      this.user = newUser
+    })
     this._favoriteService.getFavoriteObservable().subscribe((resp: any) => {
       this.favoriteQuantity = resp.items?.length;
     });
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,26 +5,44 @@ import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { HttpClient } from '@angular/common/http';
 import { USER_LOGIN_URL } from '../shared/constants/urls';
 
+const USER_KEY = 'User'
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private userSubject = new BehaviorSubject<User>(new User())
+  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage())
   public userObservable!: Observable<User>
 
-  constructor(private _http: HttpClient) { 
+  constructor(private _http: HttpClient) {
     this.userObservable = this.userSubject.asObservable()
   }
 
   login(userLogin: IUserLogin): Observable<User> {
-    console.log(userLogin, "this is user logun");
-    
+    console.log(userLogin, "this is user login");
+
     return this._http.post<User>(USER_LOGIN_URL, userLogin).pipe(
       tap({
-        next: (user) => console.log("User has sucessfully logged in", user),
-        error:(error) => console.log("An error has occured", error)
+        next: (user) => {
+          console.log("User has sucessfully logged in", user)
+          this.setUserToLocalStorage(user)
+        },
+        error: (error) => console.log("An error has occured", error)
       })
     )
   }
+
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user))
+  }
+
+  private getUserFromLocalStorage(): User {
+    const userJson = localStorage.getItem(USER_KEY);
+    if (userJson) {
+      return JSON.parse(userJson) as User
+    }
+    return new User()
+  }
 }
+
