98569d09537b3ebe08fc18ba1e1b0162a84b3547
Vishal Tripathi <rdshrma61@gmail.com>
2024-07-05 18:09:54 +0530

    changed all the api calls to backend(Atlas)

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { sample_foods, sample_tags } from '../data';
 import asyncHandler from 'express-async-handler'
 import { FoodModel } from '../models/food.modal';
@@ -6,9 +6,9 @@ import { FoodModel } from '../models/food.modal';
 const router = Router();
 
 router.get('/seed', asyncHandler(
-    async(req, resp) => {
+    async (req, resp) => {
         const foodCount = await FoodModel.countDocuments();
-        if(foodCount > 0) {
+        if (foodCount > 0) {
             resp.send("Seeding is already DONE!!");
             return;
         }
@@ -19,30 +19,90 @@ router.get('/seed', asyncHandler(
 ))
 
 // in server.ts we've defined /api/foods to the food router, so here we dont need to define that again
-router.get('/', (req, resp) => {
-    resp.send(sample_foods);
-});
 
-router.get('/search/:searchTerm', (req, resp) => {
-    const searchTerm = req.params.searchTerm;
-    const foods = sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    resp.send(foods)
-});
+router.get('/', asyncHandler(
+    async (req, resp) => {
+        const foods = await FoodModel.find(); // will get all values from database
+        resp.send(foods)
+    }
+))
+// router.get('/', (req, resp) => {
+//     resp.send(sample_foods);
+// });
 
-router.get('/tags', (req, resp) => {
-    resp.send(sample_tags)
-});
+router.get('/search/:searchTerm', asyncHandler(
+    async (req, resp) => {
+        const searchRegex = new RegExp(req.params.searchTerm, 'i') // i is for case insensitive
+        const foods = await FoodModel.find({ name: { $regex: searchRegex } })
+        resp.send(foods);
+    }
+))
 
-router.get('/tag/:tagName', (req, resp)=> {
-    const tagName = req.params.tagName;
-    const foods = sample_foods.filter(food => food.tags?.includes(tagName));
-    resp.send(foods)
-});
+// router.get('/search/:searchTerm', (req, resp) => {
+//     const searchTerm = req.params.searchTerm;
+//     const foods = sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+//     resp.send(foods)
+// });
 
-router.get('/:foodId', (req, resp) => {
-    const foodId = req.params.foodId;
-    const foods = sample_foods.find(food => food.id === foodId);
-    resp.send(foods)
-});
+// router.get('/tags', (req, resp) => {
+//     resp.send(sample_tags)
+// });
+
+router.get("/tags", asyncHandler(
+    async (req, res) => {
+        const tags = await FoodModel.aggregate([
+            {
+                $unwind: '$tags'
+            },
+            {
+                $group: {
+                    _id: '$tags',
+                    count: { $sum: 1 }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    name: '$_id',
+                    count: '$count'
+                }
+            }
+        ]).sort({ count: -1 });
+
+        const all = {
+            name: 'All',
+            count: await FoodModel.countDocuments()
+        }
+
+        tags.unshift(all);
+        res.send(tags);
+    }
+))
+
+// router.get('/tag/:tagName', (req, resp) => {
+//     const tagName = req.params.tagName;
+//     const foods = sample_foods.filter(food => food.tags?.includes(tagName));
+//     resp.send(foods)
+// });
+
+router.get('/tag/:tagName', asyncHandler(
+    async(req, resp) => {
+        const foods = await FoodModel.find({tags: req.params.tagName})
+        resp.send(foods)
+    }
+))
+
+// router.get('/:foodId', (req, resp) => {
+//     const foodId = req.params.foodId;
+//     const foods = sample_foods.find(food => food.id === foodId);
+//     resp.send(foods)
+// });
+
+router.get('/:foodId', asyncHandler(
+    async(req, resp) => {
+        const foods = await FoodModel.findById(req.params.foodId)
+        resp.send(foods);
+    }
+))
 
 export default router;
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,46 +1,47 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import jwt from "jsonwebtoken"
-import {sample_users } from '../data';
+import { sample_users } from '../data';
 import asyncHandler from 'express-async-handler';
 import { UserModel } from '../models/user.model';
 
 const router = Router();
 
 router.get('/seed', asyncHandler(
-    async(req, resp) => {
-        const usersCount = await UserModel.countDocuments();
-        if(usersCount > 0) {
-            resp.send("Seeding is already DONE");
-            return;
-        }
-        await UserModel.create(sample_users);
-        resp.send("new Seed is DONE")
+  async (req, resp) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      resp.send("Seeding is already DONE");
+      return;
     }
+    await UserModel.create(sample_users);
+    resp.send("new Seed is DONE")
+  }
 ))
 // in server.ts we've defined /api/users to the user router, so here we dont need to define that again
-router.post('/login', (req, resp) => {
-    const body = req.body; 
+router.post('/login', asyncHandler(
+  async (req, resp) => {
+    const body = req.body;
     // sent body as JSON (need to use app.use(express.json()))
-    // const {email, password} = req.body
-    const user = sample_users.find(user => user.email === body.email && user.password === body.password)
-    if(user) {
+    const {email, password} = req.body
+    const user = await UserModel.findOne({ email, password })
+    if (user) {
       resp.send(generateTokenResponse(user))
     }
     else {
       resp.status(400).send("!!! username or password is not valid !!!")
     }
-  });
-  
-  const generateTokenResponse = (user: any) => {
-    const token = jwt.sign( // it takes 3 args, what to encode, secretkey, expiry
-      {email: user.email, isAdmin: user.isAdmin},
-      "SecretKey",
-      {expiresIn: '30d'}
-    );
-  
-    user.token = token;    
-    return user;
   }
+));
 
-  export default router;
-  
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign( // it takes 3 args, what to encode, secretkey, expiry
+    { email: user.email, isAdmin: user.isAdmin },
+    "SecretKey",
+    { expiresIn: '30d' }
+  );
+
+  user.token = token;
+  return user;
+}
+
+export default router;
